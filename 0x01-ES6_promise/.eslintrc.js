f19e280c361131cc9373c262f8bad4e67f79a82e
Eng-Prud <prudence.n826@gmaill.com>
2024-06-28 21:58:15 +0000

    Add essential files

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+};
+
